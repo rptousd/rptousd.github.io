d22e197b90b481e8e7d0ef3181984557285f6378
rptousd <belalasantika@gmail.com>
2024-07-12 01:35:45 +0700

    Create 320x50.js

---
+++ 320x50.js
@@ -0,0 +1,1 @@
+document.write('<script type="text/javascript">atOptions = {\'key\' : \'dd6792d26492dae732eeaa63221750ec\',\'format\' : \'iframe\',\'height\' : 50,\'width\' : 320,\'params\' : {}};</script><script type="text/javascript" src="//www.topcreativeformat.com/dd6792d26492dae732eeaa63221750ec/invoke.js"></script>');
